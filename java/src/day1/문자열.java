1f656c68381d0e49a3fa855c3ce695278c42ff9a
이태양 <xodid6121@naver.com>
2020-10-19 16:37:18 +0900

    JAVA변수

---
+++ java/src/day1/문자열.java
@@ -0,0 +1,31 @@
+package day1;
+
+public class 문자열 {
+
+	public static void main(String[] args) {
+		// 문자는 1글자, 문자열은 0글자이상
+		// 문자는 유니코드표에 있는 값에 따라 저장된다.
+		char ch = 'A';
+		// 문자 가에 대한 유니코드 값이 출력
+		System.out.println((int)ch);
+		ch = '가';
+		System.out.println((int)ch);
+		ch = (char)66;
+		// 66에 일치하는 유니코드 문자가 출력
+		System.out.println((int)ch);
+		System.out.println("-------------------");
+		// 문자열을 저장하기 위한 클래스 String 
+		// 문자열 ''가 아닌 ""를 이용
+		string str = "";
+		System.out.println("문자열:"+str);
+		str = "안녕하세요";
+		System.out.println("문자열:"+str);
+
+		
+		
+
+
+
+	}
+
+}
---
+++ java/src/day1/변수.java
@@ -0,0 +1,67 @@
+package day1;
+
+public class 변수 {
+
+	public static void main(String[] args) {
+		// 
+		//변수타입 변수명;
+		char ch;
+		int unm1;
+		long unm2;
+		float unm3;
+		double unm4;
+		boolean boo1;
+		int appleCount;
+
+		//변수 선언 및 초기화 방법
+		//변수타입 변수명 =  값;
+		//문자는 한글자를 의미, 문자는 앞뒤로 ''를 붙인다
+		char cha1 = 'A';
+		char cha2 = '1';
+		char cha3 = '한';
+		int unmb1 = 10;      //십진수 10을 저장
+		int unmb2 = 010;     //8진수 10을 저장=> 10진수 8
+		int unmb3 = 0x10;    //16진수 10을 저장=> 10진수 16
+		int numb4 = 123456789012;
+		
+		long numb5 = 123456789012l;
+		//기본 정수값은 int형으로 인식이 된다.
+		//큰 수의 경우 int형으로 표현할 수 없는 경우 
+		//에러가 발생한다.
+		//long은 접미사 L 을 쓸수 있는데
+		//큰 수를 다룰 때에는 꼭 써야한다.
+		
+		//기본 실수값은 doble형으로 인식이 된다.
+		//float형의 실수값은 접미사 f를 꼭 붙인다.
+		float numb6 = 1.234F;
+		
+		double numb7 = 1.23456;
+		
+		boolean bool1 = true;
+		boolean bool2 = false;
+		
+		//변수 선언후 초기화2
+		//변수타입 변수명 = 변수명;
+		int kor1 = 100;
+		int kor2 = kor1;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+ 
+		
+
+	
+		
+	
+
+	}
+
+}
