5bad936f91c596b65b4aeabf84fe5d0f902982b4
이태양 <xodid6121@naver.com>
2020-10-21 17:34:12 +0900

    JAVA - for문

---
+++ java/src/day3/For문4.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class For문4 {
+
+	public static void main(String[] args) {
+		/* 1부터 100까지 2의 배수들을 출력하는 예제
+		 * */
+		int num;
+		for(num=2; num <= 100; num = num + 2) {
+			System.out.println(num + " ");
+			
+			}
+		
+		
+	}
+	
+
+}
